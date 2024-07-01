bfb196625efc2d77c388c10ee42fd66ae73e9b5d
Sushant Gangwar <gangwarsushant776@gmail.com>
2024-07-01 16:35:16 +0530

    Pushing to main

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup,find_packages
 from typing import List
+
+def get_requirements()->list[str]:
+    requirement=list[str]=[]
+    return requirement
+
 setup(
     name='Calorie Predictor App',
     version="0.0.1",
@@ -7,5 +12,5 @@ setup(
     author_email="gangwarsushant776@gmail.com",
     packages=find_packages(),
 
-    requires=["Pymongo"],
+    install_requires=["pymongo"],
 )
