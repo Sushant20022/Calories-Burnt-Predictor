2700dce16f25f6b0be484c9eafdae3215462a5ea
Sushant Gangwar <gangwarsushant776@gmail.com>
2024-07-03 20:22:01 +0530

    Added the logger.py

---
+++ Calorie/logger.py
@@ -0,0 +1,19 @@
+import logging
+import sys 
+import os
+from datetime import datetime
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FOLDER=os.path.join(os.getcwd(),LOG_FILE)
+os.makedirs(LOG_FOLDER,exist_ok=True)
+FINAL_PATH=os.path.join(LOG_FOLDER,LOG_FILE)
+
+
+logging.basicConfig(
+filename=FINAL_PATH,
+format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+level=logging.INFO,
+
+
+
+
+)
---
+++ Calorie/main.py
@@ -0,0 +1,5 @@
+from logger import logging
+try:
+    logging.info("DID THE LOGGING")
+except Exception as e:
+    print(e)
